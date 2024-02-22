6e34c6d83fd4ca9d79afb49250dbaf5ab7e61ed5
Georgi Kolev <g.t.kolev1@gmail.com>
2024-02-22 19:49:54 +0200

    Fixed Q-learning agent bugs. Added more logic

--- agents/ql_agent.py
+++ agents/ql_agent.py
@@ -1,26 +1,32 @@
+import random
+
 import torch
 import torch.nn as nn
 
 
 class QlAgent:
-    def __init__(self, input_shape=165, output_shape=2**15, loss_fn=nn.MSELoss):
+    def __init__(self, input_shape=165, output_shape=8, loss_fn=nn.MSELoss, learning_rate=1e-03, epsilon=1):
+        self.epsilon = epsilon
+        self.learning_rate = learning_rate
         self.input_shape = input_shape
         self.output_shape = output_shape
         self.model = nn.Sequential(
-            nn.Linear(input_shape, 512),
-            nn.ReLU(),
-            nn.Linear(512, 256),
+            nn.Linear(input_shape, 256),
             nn.ReLU(),
             nn.Linear(256, 128),
             nn.ReLU(),
-            nn.Linear(128, output_shape)
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, output_shape)
         )
         self.loss_fn = loss_fn
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
 
-    def take_action(self, observations):
-        if observations.shape() != (self.input_shape, ):
+    def predict_rewards(self, observations):
+        if observations.shape != (self.input_shape, ):
             raise Exception(f'Invalid input shape. Expected ${self.input_shape}, got ${observations.shape()}!')
         pred_rewards = self.model(observations)
+        return pred_rewards
 
     def learn(self, pred_reward, actual_reward):
         self.loss_fn(pred_reward, actual_reward)
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # nets/grid4x4/grid4x4.net.xml
 import re
+
+import numpy
 import sumo_rl
 import random
 import numpy as np
+import torch
+
+from agents.ql_agent import QlAgent
 
 env = sumo_rl.SumoEnvironment(net_file='nets/grid4x4/grid4x4.net.xml',
                   route_file='nets/grid4x4/grid4x4_1.rou.xml',
@@ -10,7 +15,6 @@ env = sumo_rl.SumoEnvironment(net_file='nets/grid4x4/grid4x4.net.xml',
                   num_seconds=3600,
                   single_agent=False)
 observations = env.reset()
-epochs = 500
 
 pattern1 = re.compile(r"[A-Z]\d[A-Z]\d")  # Capital letter, number, capital letter, number
 print(env.sumo.trafficlight.getIDList())
@@ -28,10 +32,42 @@ for edge in edges:
         neighbours[tl2].append(tl1)
     elif tl2 not in neighbours:
         neighbours.update({tl2: [tl1]})
+neighbours = {ts: numpy.array(neighbours[ts]) for ts in env.traffic_signals}
 print(neighbours)
-# for i in range(epochs):
-#     actions = {agent: random.randint(0, 7) for agent in env.traffic_signals}  # this is where you would insert your policy
-#     observations, rewards, done, infos = env.step(actions)
-#
-#     print('-----------------')
-#     # print(np.average(np.array(list(rewards.values()))))
+
+epochs = 5
+epsilon=1
+gamma=0.9
+
+ql_agents = {
+    ts: QlAgent()
+    for ts in env.ts_ids
+}
+
+for i in range(epochs):
+    observations = env.reset()
+    done = {"__all__": False}
+    i=0
+    while not done["__all__"]:
+        input_data = {}
+        for ts in observations:
+            concatenated_array = np.concatenate([observations[n] for n in neighbours[ts]] + [observations[ts]])
+            if len(concatenated_array) < 165:
+                padded_array = np.concatenate((concatenated_array, np.full(165 - len(concatenated_array), -1)))
+            else:
+                padded_array = concatenated_array[:165]
+            input_data[ts] = torch.tensor(padded_array).to(torch.float)
+
+        print(input_data['A1'].shape)
+        print(len(input_data['A1']))
+        pred_rewards = {ts: ql_agents[ts].predict_rewards(input_data[ts]) for ts in env.traffic_signals}
+        print(pred_rewards)
+        actions = {
+            agent:
+                random.randint(0, 7) if random.random() < epsilon
+                else torch.argmax(pred_rewards[agent], dim=1).item()
+            for agent in env.traffic_signals
+        }
+        print(actions)
+        observations, rewards, done, infos = env.step(actions)
+    # print(np.average(np.array(list(rewards.values()))))
